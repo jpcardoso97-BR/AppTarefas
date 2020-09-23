7e37b858eea217566e352fb24db7a14d264b3dc3
joao.230887 <joaopaulo.ra04@gmail.com>
2020-09-23 14:55:30 -0300

    Método adicionado

--- src/AppBlog/Postagem.java
+++ src/AppBlog/Postagem.java
@@ -1,24 +1,37 @@
 package AppBlog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Postagem {
     private Usuario nome;
     private String titulo;
     private String texto;
     private String data;
 
-    public Postagem(Usuario nome, String titulo, String texto, String data) {
+    List<Comentario> listaDeComentarios = new ArrayList<>();
+
+    public Postagem(Usuario nome, String titulo, String texto, String data, List listaDeComentarios) {
         this.nome = nome;
         this.titulo = titulo;
         this.texto = texto;
         this.data = data;
+        this.listaDeComentarios = listaDeComentarios;
     }
+
+    public Postagem(Usuario usuario, String data, String texto) {
+    }
+
+    public void addComentario(Comentario comentario){
+        listaDeComentarios.add(comentario);
+    }
+
     @Override
     public String toString() {
-        return "[" +
-                "Usuário = " + '\'' + nome+ '\''+ ", "+
-                "Titulo = "+'\''+titulo+ '\''+", "+
-                "Texto = "+'\''+texto+ '\''+", "+
-                "Data = "+'\''+data+ '\''+
+        return "Autor = "+nome+"[" +
+                "Titulo = "+titulo+']'+
+                "["+"Texto = "+texto+']'+
+                "["+"Data = "+'\''+data+']'+
                 ']';
     }
 }
