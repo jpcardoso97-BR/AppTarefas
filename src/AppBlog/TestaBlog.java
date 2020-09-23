12392661d3422f3951c98c8eac6d35bb9d41386d
joao.230887 <joaopaulo.ra04@gmail.com>
2020-09-23 17:50:21 -0300

    Update nos dados do objeto comentario4

--- src/AppBlog/TestaBlog.java
+++ src/AppBlog/TestaBlog.java
@@ -10,7 +10,7 @@ public class TestaBlog {
         Usuario usuario1 = new Usuario("Jooj", 22);
         Usuario usuario2 = new Usuario("Jooje", 21);
         Usuario usuario3 = new Usuario("Jorje", 20);
-        Usuario usuario4 = new Usuario("Joojin", 19);
+        Usuario usuario4 = new Usuario("Joojin", 10);
         Usuario usuario5 = new Usuario("Joojinilson", 55);
 
         Postagem postagem = new Postagem(usuario,"Top 10 Animes battles", "(texto)", "22/09/2020", listaDeComentarios);
@@ -18,7 +18,7 @@ public class TestaBlog {
         Comentario comentario = new Comentario(usuario1, "22/09/2020", "daora as lutas");
         Comentario comentario2 = new Comentario(usuario2, "22/09/2020", "já tinha visto");
         Comentario comentario3 = new Comentario(usuario3, "22/09/2020", "épico demais");
-        Comentario comentario4 = new Comentario(usuario4, "22/09/2020", "mim ajudem achegar nós 10 incritos wwwyoutube.com/jojinbr");
+        Comentario comentario4 = new Comentario(usuario4, "22/09/2020", "mim ajudem achegar nós 10 incritos - youtube.com/jojinbr");
         Comentario comentario5 = new Comentario(usuario5, "04/12/2020", "Não entendi a língua que eles estão falando.");
 
         listaDeComentarios.add(comentario);
