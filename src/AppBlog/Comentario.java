37be59869737c14d1dca2fee08c4206230dea0a1
joao.230887 <joaopaulo.ra04@gmail.com>
2020-09-23 14:56:28 -0300

    Adicionado um extends; Update do método construtor.

--- src/AppBlog/Comentario.java
+++ src/AppBlog/Comentario.java
@@ -1,11 +1,19 @@
 package AppBlog;
 
-public class Comentario{
+import java.util.List;
+
+public class Comentario extends Postagem{
     private Usuario nome;
     private String data;
     private String texto;
 
+    public Comentario(Usuario nome, String titulo, String texto, String data, List listaDeComentarios, Usuario nome1, String data1, String texto1) {
+        super(nome, titulo, texto, data, listaDeComentarios);
+
+    }
+
     public Comentario(Usuario nome, String data, String texto) {
+        super(nome, data, texto);
         this.nome = nome;
         this.data = data;
         this.texto = texto;
@@ -14,9 +22,9 @@ public class Comentario{
     @Override
     public String toString() {
         return "{" +
-                "Usuário = " + '\'' + nome+ '\''+ ", "+
-                "Texto = "+'\''+texto+'\''+" "+
-                "Data = "+'\''+data+'\''+
+                "Usuário = " + "(" + this.nome+")"+ " "+
+                "("+"Texto = "+this.texto+")"+" "+
+                "("+"Data = "+this.data+")"+
                 '}';
     }
 }
